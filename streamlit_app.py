b22141670b8830210635d9ac7ba2b526cafebd6f
rehpython <132597506+rehpython@users.noreply.github.com>
2023-05-04 17:36:50 +0100

    Update streamlit_app.py
    Capitalisation fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale Spinach and rocket smoothie')
-streamlit.text('Hard-Boiled Free range egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
