e651e81021be9e39427dddad4bf3bc2a3b6db80a
rehpython <132597506+rehpython@users.noreply.github.com>
2023-05-04 17:35:19 +0100

    Update streamlit_app.py
    missed a quote

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 and Blueberyy oatmeal)
+streamlit.text('Omega 3 and Blueberyy oatmeal')
 streamlit.text('Kale Spinach and rocket smoothie')
 streamlit.text('Hard-Boiled Free range egg')
