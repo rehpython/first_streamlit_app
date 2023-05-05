76089fc21edb8dba9598625f578e3b42f89c5b78
rehpython <132597506+rehpython@users.noreply.github.com>
2023-05-05 17:14:09 +0100

    Update streamlit_app.py
    added variable to choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) # just write json to the screen
 # TAKE JSON VERSION OF THE RESPONSE AND NORMALIZE IT
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
