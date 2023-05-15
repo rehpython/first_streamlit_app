cd696d49614e02f6c982f4535169114005855ff0
rehpython <132597506+rehpython@users.noreply.github.com>
2023-05-15 16:12:26 +0100

    Update streamlit_app.py
    bug - import snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake-connector
+import snowflake.connector
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
