4f9e12363ae2b152e026dc8266ccded77f808621
rehpython <132597506+rehpython@users.noreply.github.com>
2023-05-15 17:03:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ try:
    if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
    else:
-      back_from_function  = get_fruityvice_data(fruity_choice)
+      back_from_function  = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
       #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
